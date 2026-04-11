297465935f078733f51752f4543b880d03edba2e
20syldev <git@sylvain.sh>
2026-04-11 19:11:22 +0200

    MAJOR: wrkit: Add wrkit@2.2.0 package

---
+++ packages/npm/wrkit/2.2.0/dist/env.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+const required = [
+    "CLIENT_ID",
+    "TOKEN",
+    "GUILD_ID",
+    "ROLE_SLAM",
+    "ROLE_SISR",
+    "PLANNING_SLAM",
+    "PLANNING_SISR",
+];
+for (const key of required) {
+    if (!process.env[key]) {
+        throw new Error(`Variable d'environnement manquante : ${key}`);
+    }
+}
+export const env = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    TOKEN: process.env.TOKEN,
+    GUILD_ID: process.env.GUILD_ID,
+    ROLE_SLAM: process.env.ROLE_SLAM,
+    ROLE_SISR: process.env.ROLE_SISR,
+    PLANNING_SLAM: process.env.PLANNING_SLAM,
+    PLANNING_SISR: process.env.PLANNING_SISR,
+    PORT: process.env.PORT || "4000",
+    PRODUCTION: process.env.PRODUCTION || "false",
+};
+//# sourceMappingURL=env.js.map
---
+++ packages/npm/wrkit/2.2.0/dist/events/ready.d.ts
@@ -0,0 +1,3 @@
+import type { Client } from "discord.js";
+import type { SubLogger } from "@20syldev/logger.ts";
+export declare function onReady(client: Client<true>, logger: SubLogger): void;
